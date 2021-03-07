b24edfa5905f4415e19ca43c5bb08c3a0187b5cc
AnwarHemaimat <Anwarhomaimat@gmail.com>
2021-03-08 01:01:03 +0200

    again salmon js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,184 @@
-'use strict';
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-let cookieTotalsPerHour = [];
+'use strict'
 
 let seattle={
-name1='Seattle',    
-minCust= 23,
-maxCust= 65,
-avgCookiSale= 6.3,
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    workingHours:['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM'],
+    seattlePurchasedPerDay:[],
+    customerPerHourGenerator:function (minCustomer,maxCustomer){
+        let generator=Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+              return generator;
+        },
+    averagePurchased:function (customerPerHourGenerator,avgCookieSale) {
+        for (let count = 0; count < this.workingHours.length; count++) {
+            let simulatedAmounts=parseInt(this.customerPerHourGenerator()*this.avgCookieSale);
+            this.seattlePurchasedPerDay.push(simulatedAmounts);            
+        }
+    },
 }
+seattle.averagePurchased();
+let liS=document.getElementById('Seattle')
+function seattleLi(array) {
+    for (let count = 0; count < seattle.workingHours.length+1; count++) {
+        if (count < seattle.workingHours.length) {
+            let liText=document.createElement('li');
+            liText.innerText=seattle.workingHours[count]+' : ' +seattle.seattlePurchasedPerDay[count];
+            liS.appendChild(liText);
+            let sum=0;
+            let totalSum=sum+seattle.seattlePurchasedPerDay[i];
+        } else if (count < seattle.workingHours.length+1) {
+            let liText=document.createElement('li');
+            liText.innerText='Total Sum'+' : ' + 'totalSum';
+            liS.appendChild(liText);
+        } 
+    };
+}
+seattleLi();
+
 let tokyo={
-    name1='Tokyo',    
-    minCust= 3,
-    maxCust= 24,
-    avgCookiSale= 1.2,
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    workingHours:['6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM'],
+    tokyoPurchasedPerDay:[],
+    customerPerHourGenerator:function (minCustomer,maxCustomer){
+        let generator=Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+              return generator;
+        },
+    averagePurchased:function (customerPerHourGenerator,avgCookieSale) {
+        for (let count = 0; count < this.workingHours.length; count++) {
+            let simulatedAmounts=parseInt(this.customerPerHourGenerator()*this.avgCookieSale);
+            this.tokyoPurchasedPerDay.push(simulatedAmounts);            
+        }
+    },
 }
+tokyo.averagePurchased();
+let liT=document.getElementById('tokyo')
+function tokyoLi(array) {
+    for (let count = 0; count < tokyo.workingHours.length+1; count++) {
+        if (count < tokyo.workingHours.length) {
+            let liText=document.createElement('li');
+            liText.innerText=tokyo.workingHours[count]+' : ' +tokyo.tokyoPurchasedPerDay[count];
+            liT.appendChild(liText);
+            let sum=0;
+            let totalSum=sum+tokyo.tokyoPurchasedPerDay[i];
+        } else if (count < tokyo.workingHours.length+1) {
+            let liText=document.createElement('li');
+            liText.innerText='Total Sum'+' : ' + 'totalSum';
+            liT.appendChild(liText);
+        } 
+    };
+}
+tokyoLi();
+
+
 let dubai={
-    name1='Dubai',    
-    minCust= 11,
-    maxCust= 38,
-    avgCookiSale= 3.7,
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    workingHours:['6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM'],
+    dubaiPurchasedPerDay:[],
+    customerPerHourGenerator:function (minCustomer,maxCustomer){
+        let generator=Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+              return generator;
+        },
+    averagePurchased:function (customerPerHourGenerator,avgCookieSale) {
+        for (let count = 0; count < this.workingHours.length; count++) {
+            let simulatedAmounts=parseInt(this.customerPerHourGenerator()*this.avgCookieSale);
+            this.dubaiPurchasedPerDay.push(simulatedAmounts);            
+        }
+    },
 }
+dubai.averagePurchased();
+let liD=document.getElementById('dubai')
+function dubaiLi(array) {
+    for (let count = 0; count < dubai.workingHours.length+1; count++) {
+        if (count < dubai.workingHours.length) {
+            let liText=document.createElement('li');
+            liText.innerText=dubai.workingHours[count]+' : ' +dubai.dubaiPurchasedPerDay[count];
+            liD.appendChild(liText);
+            let sum=0;
+            let totalSum=sum+dubai.dubaiPurchasedPerDay[i];
+        } else if (count < dubai.workingHours.length+1) {
+            let liText=document.createElement('li');
+            liText.innerText='Total Sum'+' : ' + 'totalSum';
+            liD.appendChild(liText);
+        } 
+    };
+}
+dubaiLi();
+
+
 let paris={
-    name1='Paris',    
-    minCust= 20,
-    maxCust= 38,
-    avgCookiSale= 2.3,
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    workingHours:['6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM'],
+    parisPurchasedPerDay:[],
+    customerPerHourGenerator:function (minCustomer,maxCustomer){
+        let generator=Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+              return generator;
+        },
+    averagePurchased:function (customerPerHourGenerator,avgCookieSale) {
+        for (let count = 0; count < this.workingHours.length; count++) {
+            let simulatedAmounts=parseInt(this.customerPerHourGenerator()*this.avgCookieSale);
+            this.parisPurchasedPerDay.push(simulatedAmounts);            
+        }
+    },
 }
+paris.averagePurchased();
+let liP=document.getElementById('paris')
+function parisLi(array) {
+    for (let count = 0; count < paris.workingHours.length+1; count++) {
+        if (count < paris.workingHours.length) {
+            let liText=document.createElement('li');
+            liText.innerText=paris.workingHours[count]+' : ' +paris.parisPurchasedPerDay[count];
+            liP.appendChild(liText);
+            let sum=0;
+            let totalSum=sum+paris.parisPurchasedPerDay[count];
+        } else if (count < paris.workingHours.length+1) {
+            let liText=document.createElement('li');
+            liText.innerText='Total Sum'+' : ' + 'totalSum';
+            liP.appendChild(liText);
+        } 
+    };
+}
+parisLi();
+
+
 let lima={
-    name1='Lima',    
-    minCust= 2,
-    maxCust= 16,
-    avgCookiSale= 4.6,
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    workingHours:['6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM'],
+    limaPurchasedPerDay:[],
+    customerPerHourGenerator:function (minCustomer,maxCustomer){
+        let generator=Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+              return generator;
+        },
+    averagePurchased:function (customerPerHourGenerator,avgCookieSale) {
+        for (let count = 0; count < this.workingHours.length; count++) {
+            let simulatedAmounts=parseInt(this.customerPerHourGenerator()*this.avgCookieSale);
+            this.limaPurchasedPerDay.push(simulatedAmounts);            
+        }
+    },
 }
-
-function randomSum(min,max){
-    return Math.floor(Math.random()*(max - min + 1) + min);
+lima.averagePurchased();
+let liL=document.getElementById('lima')
+function limaLi(array) {
+    for (let count = 0; count < lima.workingHours.length+1; count++) {
+        if (count < lima.workingHours.length) {
+            let liText=document.createElement('li');
+            liText.innerText=lima.workingHours[count]+' : ' +lima.limaPurchasedPerDay[i];
+            liL.appendChild(liText);
+            let sum=0;
+            let totalSum=sum+lima.limaPurchasedPerDay[i];
+        } else if (count < lima.workingHours.length+1) {
+            let liText=document.createElement('li');
+            liText.innerText='Total Sum'+' : ' + 'totalSum';
+            liL.appendChild(liText);
+        } 
+    };
 }
-function avgSum(){
-    let avg=(max+min/2); 
-    console.log(avg);
-}
-
+limaLi();
